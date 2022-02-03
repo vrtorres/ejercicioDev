7ce7c535d6d115f95e6eeb7a1e691d06aa9c1530
Vinicio <vinicio_torresm@yahoo.com>
2022-02-03 20:25:28 +0000

    Frontend Basico

---
+++ dev/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '31d2e57255ddf0f5211b7a69581d353b61794376',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '31d2e57255ddf0f5211b7a69581d353b61794376',
+            'dev_requirement' => false,
+        ),
+        'opis/database' => array(
+            'pretty_version' => '4.2.1',
+            'version' => '4.2.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../opis/database',
+            'aliases' => array(),
+            'reference' => '5f6839e75ff04e878ec6aec880df935c3061af20',
+            'dev_requirement' => false,
+        ),
+    ),
+);
